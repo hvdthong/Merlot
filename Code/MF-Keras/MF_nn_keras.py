28fc7ec35274224d9da81272ca80989d39d1161f
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2018-03-12 15:19:58 +0800

    adding keras mf nn

--- Code/MF-Keras/MF_nn_keras.py
+++ Code/MF-Keras/MF_nn_keras.py
@@ -11,3 +11,44 @@ from IPython.display import SVG
 from keras.utils import plot_model
 from sklearn.metrics import mean_absolute_error
 
+dataset = pd.read_csv("../data/ml-100k/u.data", sep='\t', names="user_id,item_id,rating,timestamp".split(","))
+dataset.user_id = dataset.user_id.astype('category').cat.codes.values
+dataset.item_id = dataset.item_id.astype('category').cat.codes.values
+train, test = train_test_split(dataset, test_size=0.2)
+n_users, n_movies = len(dataset.user_id.unique()), len(dataset.item_id.unique())
+n_latent_factors_user = 5
+n_latent_factors_movie = 8
+
+movie_input = keras.layers.Input(shape=[1], name='Item')
+movie_embedding = keras.layers.Embedding(n_movies + 1, n_latent_factors_movie, name='Movie-Embedding')(movie_input)
+movie_vec = keras.layers.Flatten(name='FlattenMovies')(movie_embedding)
+movie_vec = keras.layers.Dropout(0.2)(movie_vec)
+
+user_input = keras.layers.Input(shape=[1], name='User')
+user_vec = keras.layers.Flatten(name='FlattenUsers')(
+    keras.layers.Embedding(n_users + 1, n_latent_factors_user, name='User-Embedding')(user_input))
+user_vec = keras.layers.Dropout(0.2)(user_vec)
+
+concat = keras.layers.concatenate([movie_vec, user_vec])
+concat_dropout = keras.layers.Dropout(0.2)(concat)
+dense = keras.layers.Dense(200, name='FullyConnected')(concat)
+dropout_1 = keras.layers.Dropout(0.2, name='Dropout')(dense)
+dense_2 = keras.layers.Dense(100, name='FullyConnected-1')(concat)
+dropout_2 = keras.layers.Dropout(0.2, name='Dropout')(dense_2)
+dense_3 = keras.layers.Dense(50, name='FullyConnected-2')(dense_2)
+dropout_3 = keras.layers.Dropout(0.2, name='Dropout')(dense_3)
+dense_4 = keras.layers.Dense(20, name='FullyConnected-3', activation='relu')(dense_3)
+
+result = keras.layers.Dense(1, activation='relu', name='Activation')(dense_4)
+adam = Adam(lr=0.005)
+model = keras.models.Model([user_input, movie_input], result)
+model.compile(optimizer=adam, loss='mean_absolute_error')
+
+plot_model(model, to_file="model_nn.png", show_shapes=True, show_layer_names=True)
+model.summary()
+
+history = model.fit([train.user_id, train.item_id], train.rating, epochs=50, verbose=0)
+y_hat_2 = np.round(model.predict([test.user_id, test.item_id]), 0)
+y_true = test.rating
+print(mean_absolute_error(y_true, y_hat_2))
+print(mean_absolute_error(y_true, model.predict([test.user_id, test.item_id])))
--- Code/MF-Keras/load_data.py
+++ Code/MF-Keras/load_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 def data_category(path_file, name):
-    if name == "SOGH_users":
-        dataset = pd.read_csv(path_file, sep=',', names="stackoverflow_user_id,github_user_id".split(","))
+    if name == "GHSO_users":
+        dataset = pd.read_csv(path_file, sep=',', names="github_user_id,stackoverflow_user_id".split(","))
         return dataset
     elif name == "user_question":
         dataset = pd.read_csv(path_file, sep=',', names="stackoverflow_user_id,stackoverflow_question_id".split(","))
@@ -17,13 +17,29 @@ def data_category(path_file, name):
 
 
 if __name__ == "__main__":
-    path_file_ = "../data/SO_GH/SOGH_users.csv"
-    users = data_category(path_file=path_file_, name="SOGH_users")
-    print users.head()
+    path_file_ = "../data/SO_GH/GHSO_users.csv"
+    users = data_category(path_file=path_file_, name="GHSO_users")
+    print users.head(), len(users)
+
+    path_file_ = "../data/SO_GH/user_repository.csv"
+    user_repository = data_category(path_file=path_file_, name="user_repository")
+    print user_repository.head(), len(user_repository)
 
     path_file_ = "../data/SO_GH/user_question.csv"
     user_question = data_category(path_file=path_file_, name="user_question")
-    print user_question.head()
-    print len(user_question)
+    print user_question.head(), len(user_question)
+    print len(user_question.stackoverflow_question_id.unique())
+    print len(user_question.stackoverflow_user_id.unique())
+    exit()
 
-    print type(users), type(user_question)
+    GH_SO_repository = user_repository.loc[
+        user_repository['github_user_id'].isin(users['github_user_id'])]
+    print GH_SO_repository.head(), len(GH_SO_repository)
+
+    SO_GH_question = user_question.loc[
+        user_question['stackoverflow_user_id'].isin(users['stackoverflow_user_id'])]
+    print SO_GH_question.head(), len(SO_GH_question)
+
+    users_SO_GH = users.loc[(users["stackoverflow_user_id"].isin(SO_GH_question["stackoverflow_user_id"])) &
+                            (users["github_user_id"].isin(GH_SO_repository["github_user_id"]))]
+    print users_SO_GH.head(), len(users_SO_GH)
